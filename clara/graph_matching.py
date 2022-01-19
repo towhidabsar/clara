9ad4fe1eb2dac879ee0604ff27c2406c1858d998
Maheen Contractor <mc1927@rit.edu>
2022-01-19 17:10:49 -0500

    updated graph matching with options

--- clara/graph_matching.py
+++ clara/graph_matching.py
@@ -1,3 +1,4 @@
+from os import system
 import networkx as nx
 import matplotlib
 import matplotlib.pyplot as plt
@@ -5,7 +6,7 @@ from itertools import permutations
 
 from clara.model import SPECIAL_VARS, Const, Var
 
-
+# find jaccard distance between two labels
 def jaccard(list1, list2):
     intersection = len(list(set(list1).intersection(list2)))
     union = (len(list1) + len(list2)) - intersection
@@ -13,7 +14,7 @@ def jaccard(list1, list2):
 
 
 class GraphMatching():
-    def __init__(self, fnc1, fnc2):
+    def __init__(self, fnc1, fnc2, option):
         self.ICF = fnc2
         self.CF = fnc1
         self.ICG = None
@@ -28,6 +29,7 @@ class GraphMatching():
         self.finalMatching = {}
         self.longer = 0
         self.removedLocs = {}
+        self.option = option
 
     def createGraphs(self):
         self.ICG = self.makeGraph(self.ICF)
@@ -35,21 +37,28 @@ class GraphMatching():
         self.CDict = self.createLocLabelDict(self.CG)
         self.ICDict = self.createLocLabelDict(self.ICG)
 
+    # creates a dictionary where the key is the location and the label is the value
     def createLocLabelDict(self, G):
         locLabels = {}
         for name, attr in list(G.nodes(data=True)):
             locLabels[name] = attr['label']
         return locLabels
 
-    def connect(self, locs, trans, labels, G):
+    # connects the graph by adding the edges
+    # locs is the list of locations
+    # trans is the dictionary of tranistions and locations
+    # labels is the label name for each node
+    # G is the graph
+    def connect(self, locs, trans, G):
         edges = []
+        # traverses through the locations to access the transitions
         for l in locs:
+            # extracts the transitions for the location
             t = trans[l]
             true = t[True]
             false = t[False]
-            label = labels[l]
-            if not label:
-                print(l)
+
+            # adds the edges to the list
             if true:
                 edges += [(l, true, True)]
             else:
@@ -58,9 +67,11 @@ class GraphMatching():
                 edges += [(l, false, False)]
             else:
                 edges += [(l, 0, False)]
+        # adds the edges to the graph
         G.add_weighted_edges_from(edges)
         return G
 
+    # gets the label value of an expression based on the type of expression
     def getLableValue(self, exp):
         if isinstance(exp, Var):
             e = exp.name.strip()
@@ -79,6 +90,8 @@ class GraphMatching():
             a += self.getLableValue(arg)
         return [name] + a
 
+    # creates a label for a node
+    # calls getLabelValue on each expression in the node and appends it to a list
     def makeLabel(self, exp):
         label = []
         for var, e in exp:
@@ -88,14 +101,22 @@ class GraphMatching():
             label += self.getLableValue(e)
         return label
 
+    # creates the graph from the function
     def makeGraph(self, fnc):
+        # the multidigraph indicates its directed and can have multiple repeated edges
         G = nx.MultiDiGraph()
+        
+        # since a transition can go to None, it gives it the location 0 as 0 can never exist
         G.add_node(0, label='None')
+        
+        # extracts the locations and dicts for descriptions, transitions and expressions 
         desc = fnc.locdescs
         trans = fnc.loctrans
         exprs = fnc.locexprs
         loc = list(desc.keys())
         allLabels = {}
+
+        # creates a node in the graph for every location with a label, line no, exp and desc
         for l in loc:
             lno = desc[l].split(" ")[-1]
             exp = exprs[l]
@@ -107,9 +128,11 @@ class GraphMatching():
 
             G.add_node(l, label=label, desc=desc[l], lno=lno, exprs=exp)
 
-        G = self.connect(loc, trans, allLabels, G)
+        # calls connect to add the edges to the graph
+        G = self.connect(loc, trans, G)
         return G
 
+    #  used to visualise the graph
     def printGraph(self, G):
         for name, attr in list(G.nodes(data=True)):
             print('loc : ', name)
@@ -123,92 +146,14 @@ class GraphMatching():
                 print(trans['weight'], ' : ', to)
             print('\n')
 
-    def matchLabels(self):
-        G1 = self.CG
-        G2 = self.ICG
-
-        len1 = len(list(G1.nodes()))
-        len2 = len(list(G2.nodes()))
-
-        labelMatrix = [[0 for _ in range(0, len1)] for _ in range(0, len2)]
-
-        for i, (_, cattr) in enumerate(list(G1.nodes(data=True))):
-            cname = cattr['label'].split(',')
-            for j, (_, iattr) in enumerate(list(G2.nodes(data=True))):
-                iname = iattr['label'].split(',')
-                labelMatrix[i][j] = jaccard(cname, iname)
-
-        self.labelMatch = labelMatrix
-
-        self.printGraph(G1)
-        print('\n')
-        self.printGraph(G2)
-        print('\n')
-        print(labelMatrix)
-
-    def matchEdges(self):
-        G1 = self.CG
-        G2 = self.ICG
-        dict1 = self.CDict
-        dict2 = self.ICDict
-        labelDist = self.labelMatch
-
-        len1 = len(list(G1.nodes()))
-        len2 = len(list(G2.nodes()))
-        edgeMatrix = [[0 for i in range(0, len1)] for i in range(0, len2)]
-        for i, (cname, cattr) in enumerate(list(G1.nodes(data=True))):
-            for j, (iname, iattr) in enumerate(list(G2.nodes(data=True))):
-                if len(cattr) == 0 and len(iattr) == 0:
-                    edgeMatrix[i][j] = 1
-                elif len(cattr) == 0:
-                    edgeMatrix[i][j] = 0
-                elif len(iattr) == 0:
-                    edgeMatrix[i][j] = 0
-                else:
-                    edges1 = list(G1.edges(cname, data=True))
-                    _, t1, _ = edges1[0]
-                    _, f1, _ = edges1[1]
-                    edges2 = list(G2.edges(iname, data=True))
-                    _, t2, _ = edges2[0]
-                    _, f2, _ = edges2[1]
-                    t1 = dict1[t1]
-                    t2 = dict2[t2]
-                    f1 = dict1[f1]
-                    f2 = dict2[f2]
-                    edgeMatrix[i][j] = 0.5*labelDist[t1][t2] + \
-                        0.5*labelDist[f1][f2]
-        self.edgeMatch = edgeMatrix
-        # print(edgeMatrix)
-
-    def createFinalSimilarity(self):
-        edges = self.edgeMatch
-        labels = self.labelMatch
-        len2 = len(edges)
-        len1 = len(edges[0])
-        matching = {}
-        final = [[0 for i in range(0, len1)] for i in range(0, len2)]
-        for i in range(0, len1):
-            temp = 0
-            index = 0
-            for j in range(0, len2):
-                val = (edges[i][j] + labels[i][j]) / 2
-                if val > temp:
-                    temp = val
-                    index = j
-                final[i][j] = (edges[i][j] + labels[i][j]) / 2
-            matching[i] = [index, temp]
-        self.finalMatrix = final
-        self.finalMatching = matching
-        print(matching)
-
+    # analyzes both the graphs and finds a matching based on label distance and edges
     def allNodeCombinations(self):
         G1 = self.CG
         G2 = self.ICG
         d1 = self.CDict
         d2 = self.ICDict
 
-        self.printGraph(G1)
-
+        # checks to see which graph is longer
         l1 = list(G1.nodes())
         l2 = list(G2.nodes())
         rev = False
@@ -221,6 +166,8 @@ class GraphMatching():
         elif len(l1) > len(l2):
             self.longer = 2
 
+        # finds all possible combinations of node matching (exhaustive list)
+        # the allComb is a list of lists where the outer list contains 1 possible matching for the entire graph
         allComb = []
         permut = permutations(l1, len(l2))
         for comb in permut:
@@ -230,9 +177,10 @@ class GraphMatching():
         bestMatch = []
         score = 0
 
-        print(G1.nodes(data=True))
+        # traversing a possible match for the graphs
         for match in allComb:
             currScore = 0
+            # traversing each node match 
             for n1, n2 in match:
                 lab1 = 0
                 lab2 = 0
@@ -244,105 +192,70 @@ class GraphMatching():
                     lab2 = d2[n2]
 
                 labelDist = jaccard(lab1.split(','), lab2.split(','))
+                if self.option != 3:
+                    edges1 = []
+                    edges2 = []
+                    if rev:
+                        edges1 = list(G1.edges(n2, data=True))
+                        edges2 = list(G2.edges(n1, data=True))
+                    else:
+                        edges1 = list(G1.edges(n1, data=True))
+                        edges2 = list(G2.edges(n2, data=True))
 
-                edges1 = []
-                edges2 = []
-                if rev:
-                    edges1 = list(G1.edges(n2, data=True))
-                    edges2 = list(G2.edges(n1, data=True))
+                    if len(edges1) == 0 and len(edges2) == 0:
+                        currScore += 0.5 + 0.5*labelDist
+                        continue
+                    elif len(edges1) == 0 or len(edges2) == 0:
+                        currScore += 0.5*labelDist
+                        continue
+
+                    _, t1, _ = edges1[0]
+                    _, f1, _ = edges1[1]
+                    _, t2, _ = edges2[0]
+                    _, f2, _ = edges2[1]
+
+                    t1 = d1[t1]
+                    t2 = d2[t2]
+                    f1 = d1[f1]
+                    f2 = d2[f2]
+
+                    t1 = t1.split(',')
+                    t2 = t2.split(',')
+                    f1 = f1.split(',')
+                    f2 = f2.split(',')
+
+                    edgeDist = 0.5*jaccard(t1, t2) + 0.5*jaccard(f1, f2)
+                    currScore += (labelDist + edgeDist) / 2
                 else:
-                    edges1 = list(G1.edges(n1, data=True))
-                    edges2 = list(G2.edges(n2, data=True))
-
-                if len(edges1) == 0 and len(edges2) == 0:
-                    currScore += 0.5 + 0.5*labelDist
-                    continue
-                elif len(edges1) == 0 or len(edges2) == 0:
-                    currScore += 0.5*labelDist
-                    continue
-
-                _, t1, _ = edges1[0]
-                _, f1, _ = edges1[1]
-                _, t2, _ = edges2[0]
-                _, f2, _ = edges2[1]
-                
-
-                # if rev:
-                #     t1 = d2[t1]
-                #     t2 = d1[t2]
-                #     f1 = d2[f1]
-                #     f2 = d1[f2]
-                # else:
-                t1 = d1[t1]
-                t2 = d2[t2]
-                f1 = d1[f1]
-                f2 = d2[f2]
-
-                t1 = t1.split(',')
-                t2 = t2.split(',')
-                f1 = f1.split(',')
-                f2 = f2.split(',')
-
-                edgeDist = 0.5*jaccard(t1, t2) + 0.5*jaccard(f1, f2)
-                currScore += (labelDist + edgeDist) / 2
-
+                    currScore += labelDist
+            # if currScore is greater than this is the best match of graphs
             if currScore > score:
                 score = currScore
                 bestMatch = match
         matching = {}
-        # d1 = self.CDict
-        # d2 = self.ICDict
 
         for n1, n2 in bestMatch:
             if rev:
                 matching[n2] = n1
             else:
                 matching[n1] = n2
-        print(matching)
         self.createModel(rev, bestMatch, matching)
 
+    # based on the correct graph and the matching, it recreates the incorrect model
+    # if the correct graph is longer, nodes are added to the incorrect graph
+    # if the correct graph is shorter, nodes are removed from the incorrect graph
     def createModel(self, rev, bestMatch, matching):
         d1 = self.CDict
-        d2 = self.ICDict
 
         G1 = self.CG
         G2 = self.ICG
-
-        # if rev:
-        #     new_e1 = {}
-        #     new_d1 = {}
-        #     new_t1 = {}
-        #     for n1, n2 in bestMatch:
-        #         if n1 == 'None' and n2 == 'None':
-        #             continue
-        #         loc2 = d2[n1]
-        #         new_e1[loc2] = G1.nodes[n2]['exprs']
-        #         new_d1[loc2] = G1.nodes[n2]['desc']
-
-        #         edges1 = list(G1.edges(n2, data=True))
-        #         _, t1, _ = edges1[0]
-        #         _, f1, _ = edges1[1]
-        #         if t1 == 'None':
-        #             t1 = None
-        #         else:
-        #             t1 = d1[t1]
-
-        #         if f1 == 'None':
-        #             f1 = None
-        #         else:
-        #             f1 = d1[f1]
-        #         new_t1[loc2] = {True: t1, False: f1}
-        #     for k in matching:
-        #         if matching[k] == 1:
-        #             self.CF.initloc = k
-        #     self.CF.locexprs = new_e1
-        #     self.CF.loctrans = new_t1
-        #     self.CF.locdescs = new_d1
-
-        # else:
+        
+        # intialize the following dicts for new expressions, descriptions and transitions of the model
         new_e2 = {}
         new_d2 = {}
         new_t2 = {}
+
+        # creates a dictionary for the mapped nodes
         match_incorr = {}
         for n1, n2 in bestMatch:
             c1 = n1
@@ -350,18 +263,10 @@ class GraphMatching():
             if rev:
                 c1 = n2
                 c2 = n1
-            # loc1 = d1[c1]
-            # loc2 = d2[c2]
             match_incorr[c2] = c1
 
-        print('\n', match_incorr, '\n', bestMatch)
-
-        # print('\n', self.ICF.loctrans)
-        # print('\n', self.ICDict)
-        print('\n', self.ICF, '\n')
-
-        print('\n', self.CF, '\n')
-
+        # based on the matches it recreates the new model
+        # n1 and n2 are location/node numbers
         for n1, n2 in bestMatch:
             c1 = n1
             c2 = n2
@@ -371,20 +276,47 @@ class GraphMatching():
             if c1 == 0 and c2 == 0:
                 continue
             loc1 = c1
+            # add all the expressions & descriptions of this location into the old one
             new_e2[loc1] = G2.nodes[c2]['exprs']
-            print(loc1, G2.nodes[c2]['desc'])
             new_d2[loc1] = G2.nodes[c2]['desc']
 
-            edges2 = list(G1.edges(loc1, data=True))
-            _, t2, _ = edges2[0]
-            _, f2, _ = edges2[1]
+            if self.option == 2:
+                # add the edges from the incorrect program
+                edges2 = list(G2.edges(c2, data=True))
+                _, t2, _ = edges2[0]
+                _, f2, _ = edges2[1]
 
-            if t2 == 0:
-                t2 = None
+                if t2 == 0:
+                    t2 = None
+                # maps t2 to its new value
+                if t2 in match_incorr:
+                    t2 = match_incorr[t2]
+                else:
+                    t2 = None
 
-            if f2 == 0:
-                f2 = None
-            new_t2[loc1] = {True: t2, False: f2}
+                if f2 == 0:
+                    f2 = None
+                # maps f2 to its new value
+                if f2 in match_incorr:
+                    f2 = match_incorr[f2]
+                else:
+                    f2 = None
+                new_t2[loc1] = {True: t2, False: f2}
+            else:
+                # add the edges from the correct program to ensure the graphs match
+                edges2 = list(G1.edges(loc1, data=True))
+                _, t2, _ = edges2[0]
+                _, f2, _ = edges2[1]
+
+                if t2 == 0:
+                    t2 = None
+
+                if f2 == 0:
+                    f2 = None
+                new_t2[loc1] = {True: t2, False: f2}
+
+        # this indicates that the correct program is longer so new locations must be added
+        # new locations are always empty
         if self.longer == 2:
             locs = d1.keys()
             extra = sorted(list(set(locs) - set(matching.keys())))
@@ -416,18 +348,13 @@ class GraphMatching():
                 else:
                     des = new_d2[trans]
                 new_d2[e] = des
+        # adds all the expressions into a list so they can be adde to the feedback
         remLocs = list(self.ICF.locexprs.keys() - match_incorr.keys())
         for l in remLocs:
             self.removedLocs[l] = (self.ICF.locexprs[l], self.ICF.locdescs[l])
+        
+        # we always start from location 1
         self.ICF.initloc = 1
         self.ICF.locexprs = new_e2
         self.ICF.loctrans = new_t2
         self.ICF.locdescs = new_d2
-
-
-        # print(new_e2)
-        print('\n', self.ICF, '\n')
-        # print('\n', self.CF.locexprs)
-        
-        # print('\n', new_t2)
-        # print('\n', self.CF.loctrans)
