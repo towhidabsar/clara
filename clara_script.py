5cc605f144c6c26d4acf410de1cb9badf1246c2d
Towhid <mac9908@rit.edu>
2022-11-21 13:28:55 -0700

    no more print

--- clara_script.py
+++ clara_script.py
@@ -40,12 +40,8 @@ def remove_unicode(fname):
     source = open(fname, 'r', encoding='utf-8')
     dir_name = os.path.dirname(fname)
     pname = os.path.basename(fname).split("_")[0]
-    # print(pname)
-    # print(dir_name)
     with open(os.path.join(dir_name,f'{pname}_solution.py'), 'w', encoding='utf-8') as mod:
         r = source.read().replace('\u00a0','')
-        # r = r.decode('utf-8')
-        # .replace('\u00a0','')
         mod.write(r)
     source.close()
 
@@ -111,7 +107,6 @@ def batch_run_json(problem, correct, problems, correct_path, incorrect_path, gra
             idx = results.new(cfile)
             cdired = correct_path + cfile + '_solution.py'
             idired = incorrect_path + ifile + '_solution.py'
-            # print(problem,' ', cfile, ' ', ifile)
             # go through each graph matching options
             for g in graph_matching_options:
 
@@ -123,7 +118,6 @@ def batch_run_json(problem, correct, problems, correct_path, incorrect_path, gra
                     clara_call = subprocess.run(['clara graph ' + cdired + ' ' + idired + ' --argsfile ' + testcase + ' --checkAllRep 1 --verbose 1 --matchOp ' + str(g)],
                                                 capture_output=True,
                                                 shell=True)
-                print(clara_call.stdout)
                 output = clara_call.stdout.decode('utf-8')
                 results.add(idx,"First Output", output)
                 err = clara_call.stderr.decode('utf-8')
@@ -267,7 +261,6 @@ def thread_run(lst, thread):
         if problem_name not in ['ProblemList.txt', 'SolutionLists']:
             # problem_name = '1A'
             testcase = f'/data/ScrapeData/{problem_name}/testcases/'
-            # print(problem_name)
 
             correct_path = path + problem_name + '/OK/python.3/'
             incorrect_path = path + problem_name + '/REJECTED/python.3/'
@@ -280,12 +273,10 @@ def thread_run(lst, thread):
 
 
 def main(lst, thread_num=6):
-    # print(len(lst))
     threads = list()
     splits = [[] for i in range(thread_num)]
     # Ceiling division
     per_array = -(len(lst)// -thread_num)
-    # print(per_array)
     j = 0
     for i, problem_name in enumerate(lst):
         splits[j].append(problem_name)
